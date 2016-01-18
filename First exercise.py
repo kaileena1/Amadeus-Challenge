73442c873cf7d0d1fb249bf6a1ba1d4f681c352c
kaileena <mihaela.mironescu@yahoo.com>
2016-01-18 17:59:02 +0200

    exercices

---
+++ First exercise.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[2]:
+
+num_lines1 = sum(1 for line in open('E:\motivational me Bucharest\datasets\Bookings.csv'))
+num_lines2 = sum(1 for line in open('E:\motivational me Bucharest\datasets\searches.csv'))
+
+print num_lines1
+print " "
+print num_lines2
+print ("done")
+
+
+# In[ ]:
+
+print ("here")
+
